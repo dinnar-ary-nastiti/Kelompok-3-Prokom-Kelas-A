12eea06e40d27b46cf37568598a5d09dc1b8cc2c
AmaradhikaPutriAgustina <amaradhikaputri@student.uns.ac.id>
2024-12-07 15:58:19 +0700

    Add files via upload

---
+++ mainfix.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from login_karyawan import login_karyawan
+from login_hrd import login_hrd
+from rekap import tampilkan_rekap_absensi, tampilkan_rekap_gaji

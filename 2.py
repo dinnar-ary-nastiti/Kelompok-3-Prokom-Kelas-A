ce207929df3914a5aaf42560ab5b2af05d97ebed
Dinnar Ary Nastiti <dinnararynastiti@student.uns.ac.id>
2024-12-07 18:30:52 +0700

    menghapus file tidak terpakai

--- 2.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-
-# Fungsi untuk menambahkan frame dengan border dan judul
-def create_titled_frame(parent, title):
-    frame = tk.Frame(parent, bg="lightgray", bd=2, relief="ridge", padx=10, pady=10)
-    label = tk.Label(frame, text=title, font=("Helvetica", 16, "bold"), bg="lightgray")
-    label.pack(side="top", fill="x", pady=5)
-    return frame

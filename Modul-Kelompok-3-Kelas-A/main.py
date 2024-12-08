2d09828b2c8fcb15d56ccc0433387fb148eb0545
Dinnar Ary Nastiti <dinnararynastiti@student.uns.ac.id>
2024-12-08 09:46:42 +0700

    menambahkan main.py dan def rekap

---
+++ Modul-Kelompok-3-Kelas-A/main.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+from PIL import Image, ImageTk
+from login_pekerja import login_karyawan
+from login_hrd import login_hrd, hrd_dashboard
+from rekap import tampilkan_rekap_absensi, tampilkan_rekap_gaji
